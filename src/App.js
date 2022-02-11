b72638bddd0017bf6120453f2f1bf6a2982def01
Shrish Sharma <shrish0608@gmail.com>
2022-02-11 20:13:52 +0530

    CHanged Theme Palette

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, createTheme } from "@mui/material";
 import Header from "./components/Header";
 import MainForm from "./components/MainForm";
 import Navbar from "./components/Navbar";
+import {pink} from '@mui/material/colors';
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    primary: pink
+  }
+}); 
 
 function App() {
   return (
-    <Box>
-      <Navbar />
-      <Header />
-      <MainForm />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Navbar />
+        <Header />
+        <MainForm />
+      </Box>
+    </ThemeProvider>
   );
 }
 
