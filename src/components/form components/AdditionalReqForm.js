6d7672716f5b6404c1d7229a315350e3ef6989be
Shrish Sharma <shrish0608@gmail.com>
2022-02-11 19:38:28 +0530

    Fixed Last Step Form

--- src/components/form components/AdditionalReqForm.js
+++ src/components/form components/AdditionalReqForm.js
@@ -17,9 +17,8 @@ const AdditionalReqForm = () => {
                 <FormLabel>If previous Driving License was issued from different city than current place of residence ?</FormLabel>
                 <RadioGroup
                     onChange={(e) => setAddonPref(e.target.value)}
-                    row
                     value={addOnPref}
-                    sx={{display: 'flex', justifyContent: 'center', mt: '12px'}}
+                    sx={{margin: {sm: "15px 600px 0 600px", xs: "15px 20px 0 20px"}}}
                 >
                     <FormControlLabel value="NOC available from previous city" control={<Radio />} label="NOC available from previous city" />
                     <FormControlLabel value="NOC not available from previous city" control={<Radio />} label="NOC not available from previous city" />
